7b39648507fcc0065141322a068c7fbf738758cf
Fodor Zoltan <fodor18zoltan@gmail.com>
2021-10-27 22:18:52 +0300

    Attempt replicate.

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -11,17 +11,21 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
-const installLogsPrinter = require('cypress-terminal-report/src/installLogsPrinter');
-
+const path = require('path');
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  installLogsPrinter(on, {
-    printLogsToConsole: "always",
-    collectTestLogs: () => console.log('a'),
-  });
+  const options = {
+    outputRoot: config.projectRoot + '/cypress/logs/',
+    printLogsToFile: 'always',
+    specRoot: path.relative(config.fileServerFolder, config.integrationFolder),
+    outputTarget: {
+      'cypress-logs|json': 'json',
+    },
+    logToFilesOnAfterRun: true,
+  }
+  require('cypress-terminal-report/src/installLogsPrinter')(on, options)
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
