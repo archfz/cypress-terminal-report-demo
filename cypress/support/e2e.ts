364262cfd3600f2a77af49146b2619963d697917
James Harris <wopian@wopian.me>
2022-07-12 13:37:54 +0100

    refactor: consistent use of ESM imports

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-require('cypress-terminal-report/src/installLogsCollector')();
+import installLogsCollector from 'cypress-terminal-report/src/installLogsCollector'
+
+installLogsCollector()
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
